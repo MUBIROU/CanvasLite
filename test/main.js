c29c73a5168da5fda30f525d1a06bb6096013d2a
TakashiNishimura <drm5bd01@gmail.com>
2017-10-18 11:31:49 +0900

    Update

--- test/main.js
+++ test/main.js
@@ -32,74 +32,91 @@ mouseup_canvas = (_canvas) => {
         _grid.in(2); //初期値2（秒）
         _grid.addEventListener("in", in_grid);
         _gridStatus = "animate"; //Gridの表示状態
+
+        _blockWidth = _canvas.width / 17;
+        _blockHeight = _canvas.height / 9;
+    
+        //ボタン1の表示準備
+        _button1 = new BitmapPlus("red.png", true, "255,255,255", "0,0,0",4);
+        _button1.name = "button1";
+        _button1.addEventListener("mouseup", mouseup_button);
+        _button1.addEventListener("delete", delete_button);
+        _button1.x = _blockWidth*2;
+        _button1.y = _blockHeight*3;
+        _canvas.addChild(_button1);
+    
+        //ボタン2の表示準備
+        _button2 = new BitmapPlus("red.png", true, "255,255,255", "0,0,0",4);
+        _button2.name = "button2";
+        _button2.addEventListener("mouseup", mouseup_button);
+        _button2.addEventListener("delete", delete_button);
+        _button2.x = _blockWidth*7;
+        _button2.y = _blockHeight*3;
+        _canvas.addChild(_button2);
+    
+        //ボタン3の表示準備
+        _button3 = new BitmapPlus("red.png", true, "255,255,255", "0,0,0",4);
+        _button3.name = "button3";
+        _button3.addEventListener("mouseup", mouseup_button);
+        _button3.addEventListener("delete", delete_button);
+        _button3.x = _blockWidth*12;
+        _button3.y = _blockHeight*3;
+        _canvas.addChild(_button3);
+
+        //ボタンの表示開始
+        _button1._timerID = setTimeout(callback_button_in, 650, _button1);
+        _button2._timerID = setTimeout(callback_button_in, 980, _button2);
+        _button3._timerID = setTimeout(callback_button_in, 1290, _button3);
     }
 }
 
 in_grid = (_grid) => {
     _gridStatus = "on"; //Gridの表示状態
     _grid.removeEventListener("in");
-
-    _blockWidth = _canvas.width / 17;
-    _blockHeight = _canvas.height / 9;
-
-    //ボタン1の表示準備
-    _button1 = new BitmapPlus("red.png", true, "255,255,255", "0,0,0",4);
-    _button1.name = "button1";
-    _button1.addEventListener("mouseup", mouseup_button);
-    _button1.addEventListener("delete", delete_button);
-    _button1.x = _blockWidth*2;
-    _button1.y = _blockHeight*3;
-    _canvas.addChild(_button1);
-    _button1.in(1); //ボタンの表示開始（初期値1）
-
-    //ボタン2の表示準備
-    _button2 = new BitmapPlus("red.png", true, "255,255,255", "0,0,0",4);
-    _button2.name = "button2";
-    _button2.addEventListener("mouseup", mouseup_button);
-    _button2.addEventListener("delete", delete_button);
-    _button2.x = _blockWidth*7;
-    _button2.y = _blockHeight*3;
-    _canvas.addChild(_button2);
-    _button2.in(1); //ボタンの表示開始（初期値1）
-
-    //ボタン3の表示準備
-    _button3 = new BitmapPlus("red.png", true, "255,255,255", "0,0,0",4);
-    _button3.name = "button3";
-    _button3.addEventListener("mouseup", mouseup_button);
-    _button3.addEventListener("delete", delete_button);
-    _button3.x = _blockWidth*12;
-    _button3.y = _blockHeight*3;
-    _canvas.addChild(_button3);
-    _button3.in(1); //ボタンの表示開始（初期値1）
 }
 
 out_grid = (_grid) => {
     _gridStatus = "off";
     _grid.removeEventListener("in");
-    //console.log(_canvas);
 }
 
-mouseup_button = (_bitmap) => {
-    switch (_bitmap.name) {
+mouseup_button = (_button) => {
+    _grid._timerID = setTimeout(callback_grid_out, 1520);
+
+    switch (_button.name) {
         case "button1":
-            _button2.out(1);
-            _button3.out(1);
+            _button2._timerID = setTimeout(callback_button_out, 300, _button2);
+            _button3._timerID = setTimeout(callback_button_out, 600, _button3);
+            _lastButton = _button3;
             break;
         case "button2":
-            _button1.out(1);
-            _button3.out(1);
+            _button1._timerID = setTimeout(callback_button_out, 600, _button1);
+            _button3._timerID = setTimeout(callback_button_out, 600, _button3);
+            _lastButton = _button3;
             break;
         case "button3":
-            _button1.out(1);
-            _button2.out(1);
+            _button1._timerID = setTimeout(callback_button_out, 600, _button1);
+            _button2._timerID = setTimeout(callback_button_out, 300, _button2);
+            _lastButton = _button1;
     }
 }
 
-delete_button = (_bitmap) => {
-    _canvas.deleteChild(_button1);
-    _canvas.deleteChild(_button2);
-    _canvas.deleteChild(_button3);
-    _grid.out(); //初期値2（秒）
+delete_button = (_button) => {
+    _canvas.deleteChild(_button);
+}
+
+callback_button_in = (_button) => {
+    _button.in(1);
+    clearTimeout(_button._timerID);
+}
+
+callback_button_out = (_button) => {
+    _button.out(1);
+    clearTimeout(_button._timerID);
+}
+
+callback_grid_out = () => {
+    _grid.out(1); //初期値2（秒）
     _grid.addEventListener("out", out_grid);
     _gridStatus = "animate"; //Gridの表示状態
 }
