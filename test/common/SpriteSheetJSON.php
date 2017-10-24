b64ec3a8d8440c202d10d6841cbb63d0173994c5
TakashiNishimura <drm5bd01@gmail.com>
2017-10-24 15:27:35 +0900

    Update

--- test/common/SpriteSheetJSON.php
+++ test/common/SpriteSheetJSON.php
@@ -1,18 +1,19 @@
 <?php 
-    $w = 240; //frameWidth
-    $h = 256; //frameHeight
-    $layersNum = 9; //layersNum(framesNum)
+	$w = 580; //frameWidth
+	$h = 593; //frameHeight
+	$layerNum = 30; //layersNum(framesNum)
+	$spalten = 7; //スプライトシートの横方向の画像数
 
-    $frames = array();
-    for ($i = 0; $i < $layersNum; $i++) {
-        $frame = array();
+	$frames = array(); 
+	for ($i = 0; $i < $layerNum; $i++) { 
+        $frame = array(); 
         $frame["num"] = $i + 1;
-        $frame["x"] = $i * $w;
-        $frame["y"] = 0;
-        $frame["w"] = $w;
-        $frame["h"] = $h;
-        array_push($frames, array("frame"=>$frame));
-    } 
-    $result = array("frames"=>$frames); 
-    echo json_encode($result);
+		$frame["x"] = $i % $spalten * $w; 
+		$frame["y"] = floor($i / $spalten) * $h; 
+		$frame["w"] = $w; 
+		$frame["h"] = $h; 
+		array_push($frames, array("frame"=>$frame)); 
+	} 
+	$result = array("frames"=>$frames); 
+	echo json_encode($result); 
 ?>
